06942ef447247c0ce9c42c61550644d4751a630a
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-12 08:59:05 +0200

    Added sax parser for reading

--- src/XMLParser.cpp
+++ src/XMLParser.cpp
@@ -1,4 +1,12 @@
+#include <xercesc/parsers/SAXParser.hpp>
+#include <xercesc/util/OutOfMemoryException.hpp>
+
 #include "XMLParser.h"
+
+#include <iostream>
+
+XERCES_CPP_NAMESPACE_USE
+using namespace std;
 
 std::string XMLParser::toString() {
     return this->filename;
@@ -7,3 +15,76 @@ std::string XMLParser::toString() {
 XMLParser::XMLParser(const std::string &filename) {
     this->filename = filename;
 }
+
+void XMLParser::parse() {
+    try {
+        XMLPlatformUtils::Initialize();
+    }
+    catch (const XMLException& toCatch) {
+        char* message = XMLString::transcode(toCatch.getMessage());
+        cerr << "Error during initialization! :\n"
+             << message << "\n";
+        XMLString::release(&message);
+    }
+
+    SAXParser* parser = {nullptr};
+    parser = new SAXParser;
+
+    int errorCount = {0};
+
+    try
+    {
+        //Das eigentliche Parsen der Datei
+        parser->setDocumentHandler(this);
+        parser->parse(this->filename.c_str());
+        errorCount = parser->getErrorCount();
+    }
+    catch (const OutOfMemoryException&)
+    {
+        XERCES_STD_QUALIFIER cerr << "OutOfMemoryException" << XERCES_STD_QUALIFIER endl;
+    }
+    catch (const XMLException& toCatch)
+    {
+        char* message = XMLString::transcode(toCatch.getMessage());
+
+        //XMLString::release(message);
+        /*
+        XERCES_STD_QUALIFIER cerr << "\nAn error occurred\n  Error: "
+                                  << StrX(toCatch.getMessage())
+                                  << "\n" << XERCES_STD_QUALIFIER endl;
+                                  */
+        cerr << "XMLException: " << message << endl;
+    }
+    catch(...) {
+        cerr << "Unbekannter Fehler" << endl;
+    }
+
+    cout << "Anzahl Fehler: " << errorCount << endl;
+
+    //Parser sauber beenden
+    delete parser;
+    //Terminate muss immer am Schluss stehen
+    XMLPlatformUtils::Terminate();
+}
+
+void XMLParser::startDocument() {
+    cout << "Start Document" << endl;
+    sm.setState(State::START);
+}
+
+void XMLParser::endDocument() {
+    cout << "End Document" << endl;
+}
+
+void XMLParser::startElement(const XMLCh *const name, AttributeList &attributes) {
+    cout << "Start-Element: " << converter.to_bytes(name) << endl;
+}
+
+void XMLParser::endElement(const XMLCh *const name) {
+    cout << "End-Element: " << converter.to_bytes(name) << endl;
+}
+
+void XMLParser::characters(const XMLCh *const chars, const XMLSize_t length) {
+    cout << "Characters (" << length << "):" << converter.to_bytes(chars, chars + length) << endl;
+}
+
--- src/XMLParser.h
+++ src/XMLParser.h
@@ -1,29 +1,50 @@
+#include <xercesc/sax/HandlerBase.hpp>
+XERCES_CPP_NAMESPACE_USE
+
 #ifndef PROGRAMMING_C_XMLPARSER_H
 #define PROGRAMMING_C_XMLPARSER_H
 
 
 #include <string>
+#include <codecvt>
+#include <locale>
+#include "StateMachine.h"
+#include "models/GetOptSetup.h"
 
-class XMLParser {
+using namespace std;
+
+class XMLParser : public HandlerBase {
 private:
     /**
      * @brief filename
      * Name of the file to parse.
      */
     std::string filename;
+
+    StateMachine sm = StateMachine();
+
+    wstring_convert<codecvt_utf8_utf16<char16_t>,char16_t> converter;
 public:
     /**
      * @brief XMLParser
      * Constructor.
      * @param filename
      */
-    explicit XMLParser(const std::string &filename);
+    explicit XMLParser(const string &filename);
 
     /**
      * @brief toString
      * @return
      */
     std::string toString();
+
+    void parse();
+
+    void startDocument() override;
+    void endDocument() override;
+    void startElement(const XMLCh* const name, AttributeList& attributes) override;
+    void endElement(const XMLCh* const name) override;
+    void characters(const XMLCh* const chars, const XMLSize_t length) override;
 };
 
 
