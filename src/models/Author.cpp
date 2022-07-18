98fcbf67c48bca181cd1f19bbb2738ece4fcb0c9
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-18 09:02:37 +0200

    Changed variable naming and added parseAttributes to author

--- src/models/Author.cpp
+++ src/models/Author.cpp
@@ -2,6 +2,7 @@
 // Created by tobig on 6/10/2022.
 //
 
+#include <xercesc/util/XMLString.hpp>
 #include "Author.h"
 
 // Constructors
@@ -17,8 +18,8 @@ const string &Author::getPhone() const {
     return phone;
 }
 
-const string &Author::getEmail() const {
-    return email;
+const string &Author::getMail() const {
+    return mail;
 }
 
 
@@ -31,8 +32,20 @@ void Author::setPhone(const string &phone) {
     Author::phone = phone;
 }
 
-void Author::setEmail(const string &email) {
-    Author::email = email;
+void Author::setMail(const string &mail) {
+    Author::mail = mail;
 }
 
 
+// Helpers
+void Author::parseAttributes(AttributeList &attributes) {
+    for (unsigned int i = 0; i < attributes.getLength(); i++) {
+        if (!XMLString::compareString(attributes.getName(i), u"Name")) {
+            setName(std::string(XMLString::transcode(attributes.getValue(i))));
+        } else if (!XMLString::compareString(attributes.getName(i), u"Phone")) {
+            setPhone(std::string(XMLString::transcode(attributes.getValue(i))));
+        } else if (!XMLString::compareString(attributes.getName(i), u"Mail")) {
+            setMail(std::string(XMLString::transcode(attributes.getValue(i))));
+        }
+    }
+}
