09e697810ee805692dcdde4cede59ca9afeb22f4
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-19 19:34:30 +0200

    Added example compiled program with getopts

---
+++ src2/COptionParser.h
@@ -0,0 +1,14 @@
+#ifndef CODEGENERATOR_COPTIONPARSER_H
+#define CODEGENERATOR_COPTIONPARSER_H
+
+#include <getopt.h>
+#include <cstdio>     /* for printf */
+#include <cstdlib>    /* for exit */
+
+namespace DHBW {
+
+    class COptionParser {
+    };
+}
+
+#endif //CODEGENERATOR_COPTIONPARSER_H
