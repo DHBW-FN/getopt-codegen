d11949a3433a89d58689d16a43cbf12b8f530dcd
philippkuest <philipp.kuest@t-online.de>
2022-07-23 13:45:56 +0000

    changes

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main() {
-    XMLParser parser("example/Example.xml");
+    XMLParser parser("src3/exampleProgram.xml");
     std::cout << parser.toString() << std::endl;
     parser.parse();
     SourceCodeWriter writer = SourceCodeWriter(parser.getGetOptSetup());
--- src3/ExampleProgram.cpp
+++ src3/ExampleProgram.cpp
@@ -10,8 +10,19 @@ ExtendedGeneratedCode *ExampleProgram::getExtendedGeneratedCode() {
     return extendedGeneratedCode;
 }
 
+void checkExclusion(ExampleProgram ep){
+    if(ep.getExtendedGeneratedCode()->isSetExclusion()){
+        printf("The option 'exclusion' was called --> isSetExclusion=true");
+    } else {
+        printf("The option 'exclusion' was not called --> isSetExclusion=false");
+    }
+}
+
 int main(int argc, char* argv[] ) {
     ExampleProgram exampleProgram = ExampleProgram();
     exampleProgram.getExtendedGeneratedCode()->parseOptions(argc, argv);
+
+    checkExclusion(exampleProgram);
+
     return 0;
 }
