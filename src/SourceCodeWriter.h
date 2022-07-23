1b1b336c2b79ee8a68c7a61be2d456300eede1bf
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-23 15:51:08 +0200

    Added initialization to pointer

--- src/SourceCodeWriter.h
+++ src/SourceCodeWriter.h
@@ -10,7 +10,7 @@
 
 class SourceCodeWriter {
 private:
-    GetOptSetup *getOptSetup;
+    GetOptSetup *getOptSetup = nullptr;
     FILE *headerFile = nullptr;
     FILE *sourceFile = nullptr;
 public:
