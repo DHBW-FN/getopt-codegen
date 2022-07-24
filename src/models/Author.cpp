283fead7c62dca3464cf10fdabfe300d4e3719be
philippkust <philipp.kuest@t-online.de>
2022-07-24 12:03:21 +0200

    Added doxygen

--- src/models/Author.cpp
+++ src/models/Author.cpp
@@ -8,38 +8,34 @@
 //Constructor
 Author::Author() = default;
 
-//Getter
+//Getters
 const string &Author::getName() const {
     return name;
 }
 
-//Getter
 const string &Author::getPhone() const {
     return phone;
 }
 
-//Getter
 const string &Author::getMail() const {
     return mail;
 }
 
 
-//Setter
+//Setters
 void Author::setName(const string &_name) {
     Author::name = _name;
 }
 
-//Setter
 void Author::setPhone(const string &_phone) {
     Author::phone = _phone;
 }
 
-//Setter
 void Author::setMail(const string &_mail) {
     Author::mail = _mail;
 }
 
-
+//helper function to parse the Author-Tag
 void Author::parseAttributes(AttributeList &attributes) {
     for (unsigned int i = 0; i < attributes.getLength(); i++) {
         if (!XMLString::compareString(attributes.getName(i), u"Name")) {
